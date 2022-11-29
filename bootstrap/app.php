c694c0a8f4185ec964877ec5286bfcdf3c07e6ba
arminsadeghian <eng.a.sadeghian@gmail.com>
2022-11-29 13:37:15 +0330

    Add api versioning folder structure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
     dirname(__DIR__)
@@ -109,7 +109,9 @@ $app->configure('app');
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
 ], function ($router) {
-    require __DIR__.'/../routes/web.php';
+    require __DIR__ . '/../routes/web.php';
+    require __DIR__ . '/../routes/api/v1.php';
+    require __DIR__ . '/../routes/api/v2.php';
 });
 
 return $app;
