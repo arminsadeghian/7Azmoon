e0edc7b60a3478a22ad7a55585caf860acb5f99a
arminsadeghian <eng.a.sadeghian@gmail.com>
2022-12-05 12:01:04 +0330

    Implement delete category

--- tests/API/V1/CategoriesTest.php
+++ tests/API/V1/CategoriesTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\API\V1;
 
+use App\Repositories\Contracts\CategoryRepositoryInterface;
 use Tests\TestCase;
 
 class CategoriesTest extends TestCase
@@ -31,6 +32,40 @@ class CategoriesTest extends TestCase
                 'slug',
             ],
         ]);
+    }
+
+    /** @test */
+    public function ensureWeCanDeleteCategory()
+    {
+        $category = $this->createCategory()[0];
+        $response = $this->call('DELETE', 'api/v1/categories', [
+            'id' => $category->getId(),
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->seeJsonStructure([
+            'success',
+            'message',
+            'data',
+        ]);
+    }
+
+    private function createCategory(int $count = 1): array
+    {
+        $categories = [];
+
+        $categoryRepository = $this->app->make(CategoryRepositoryInterface::class);
+
+        $categoryData = [
+            'name' => 'category1',
+            'slug' => 'category-1',
+        ];
+
+        foreach (range(0, $count) as $item) {
+            $categories[] = $categoryRepository->create($categoryData);
+        }
+
+        return $categories;
     }
 
 }
