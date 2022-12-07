06772b839a693d20ab2d8753073aeea6bb1d2e5c
arminsadeghian <eng.a.sadeghian@gmail.com>
2022-12-07 18:10:38 +0330

    Implement documentation of categories

--- app/Http/Controllers/API/V1/CategoriesController.php
+++ app/Http/Controllers/API/V1/CategoriesController.php
@@ -13,6 +13,50 @@ class CategoriesController extends BaseAPIController
     {
     }
 
+    /**
+     *
+     * @OA\Get (
+     *     path="/api/v1/categories",
+     *     description="Returns all categories",
+     *     tags={"categories"},
+     *
+     *     @OA\Parameter (
+     *          name="page",
+     *          in="path",
+     *          description="Specify the page number",
+     *          required=false,
+     *          @OA\Schema (type="integer")
+     *     ),
+     *
+     *     @OA\Parameter (
+     *          name="page_size",
+     *          in="path",
+     *          description="Specify the number of results per page",
+     *          required=false,
+     *          @OA\Schema (type="integer")
+     *     ),
+     *
+     *     @OA\Response (
+     *          response=200,
+     *          description="All categories",
+     *          @OA\JsonContent (
+     *              @OA\Property (property="success", type="boolean", example="true"),
+     *              @OA\Property (property="message", type="string", example="All Categories"),
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="array",
+     *                  @OA\Items(
+     *                      @OA\Property(property="id", type="integer", example="1"),
+     *                      @OA\Property(property="name", type="string", example="Category Name"),
+     *                      @OA\Property(property="slug", type="string", example="category-slug"),
+     *                  )
+     *              )
+     *          ),
+     *     ),
+     *
+     *)
+     *
+     */
     public function index(Request $request)
     {
         $this->validate($request, [
@@ -25,43 +69,44 @@ class CategoriesController extends BaseAPIController
         return $this->respondSuccess('دسته بندی ها', $categories);
     }
 
-    public function store(Request $request)
-    {
-        $this->validate($request, [
-            'name' => 'required|string|min:3|max:255',
-            'slug' => 'required|string|min:3|max:255',
-        ]);
-
-        $createdCategory = $this->categoryRepository->create([
-            'name' => $request->name,
-            'slug' => $request->slug,
-        ]);
-
-        return $this->respondCreated('دسته بندی ایجاد شد', [
-            'name' => $createdCategory->getName(),
-            'slug' => $createdCategory->getSlug(),
-        ]);
-    }
-
-    public function delete(Request $request)
-    {
-        $this->validate($request, [
-            'id' => 'required|numeric',
-        ]);
-
-        if (!$this->categoryRepository->find($request->id)) {
-            return $this->respondNotFound('دسته بندی وجود ندارد');
-        }
-
-        if (!$this->categoryRepository->delete($request->id)) {
-            return $this->respondInternalError('دسته بندی حذف نشد');
-        }
-
-        $this->categoryRepository->delete($request->id);
-
-        return $this->respondSuccess('دسته بندی حذف شد', []);
-    }
-
+    /**
+     *
+     * @OA\Put (
+     *     path="/api/v1/categories",
+     *     description="Update a category",
+     *     tags={"categories"},
+     *
+     *     @OA\RequestBody (
+     *          required=true,
+     *          description="Update a category",
+     *          @OA\JsonContent (
+     *              @OA\Property (property="id", type="integer", example="1"),
+     *              @OA\Property (property="name", type="string", example="Category Name"),
+     *              @OA\Property (property="slug", type="string", example="category-slug"),
+     *          ),
+     *      ),
+     *
+     *     @OA\Response (
+     *          response=200,
+     *          description="Category updated",
+     *          @OA\JsonContent (
+     *              @OA\Property (property="success", type="boolean", example="true"),
+     *              @OA\Property (property="message", type="string", example="Category updated"),
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="array",
+     *                  @OA\Items(
+     *                      @OA\Property(property="id", type="integer", example="1"),
+     *                      @OA\Property(property="name", type="string", example="New Category"),
+     *                      @OA\Property(property="slug", type="string", example="new-slug"),
+     *                  )
+     *              )
+     *          ),
+     *     ),
+     *
+     * )
+     *
+     */
     public function update(Request $request)
     {
         $this->validate($request, [
@@ -83,6 +128,108 @@ class CategoriesController extends BaseAPIController
             'name' => $updatedCategory->getName(),
             'slug' => $updatedCategory->getSlug(),
         ]);
+    }
+
+    /**
+     *
+     * @OA\Post (
+     *     path="/api/v1/categories",
+     *     description="Create new category",
+     *     tags={"categories"},
+     *
+     *     @OA\RequestBody (
+     *          required=true,
+     *          description="Create new category",
+     *          @OA\JsonContent (
+     *              @OA\Property (property="name", type="string", example="Category Name"),
+     *              @OA\Property (property="slug", type="string", example="category-slug"),
+     *          ),
+     *      ),
+     *
+     *     @OA\Response (
+     *          response=201,
+     *          description="Category created",
+     *          @OA\JsonContent (
+     *              @OA\Property (property="success", type="boolean", example="true"),
+     *              @OA\Property (property="message", type="string", example="All Categories"),
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="array",
+     *                  @OA\Items(
+     *                      @OA\Property(property="id", type="integer", example="1"),
+     *                      @OA\Property(property="name", type="string", example="Category Name"),
+     *                      @OA\Property(property="slug", type="string", example="category-slug"),
+     *                  )
+     *              )
+     *          ),
+     *     ),
+     *
+     * )
+     *
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|min:3|max:255',
+            'slug' => 'required|string|min:3|max:255',
+        ]);
+
+        $createdCategory = $this->categoryRepository->create([
+            'name' => $request->name,
+            'slug' => $request->slug,
+        ]);
+
+        return $this->respondCreated('دسته بندی ایجاد شد', [
+            'id' => $createdCategory->getId(),
+            'name' => $createdCategory->getName(),
+            'slug' => $createdCategory->getSlug(),
+        ]);
+    }
+
+    /**
+     *
+     * @OA\Delete  (
+     *     path="/api/v1/categories",
+     *     description="Delete a category",
+     *     tags={"categories"},
+     *
+     *     @OA\RequestBody (
+     *          required=true,
+     *          description="Delete a category",
+     *          @OA\JsonContent (
+     *              @OA\Property (property="id", type="integer", example="1"),
+     *          ),
+     *      ),
+     *
+     *     @OA\Response (
+     *          response=200,
+     *          description="Category deleted",
+     *          @OA\JsonContent (
+     *              @OA\Property (property="success", type="boolean", example="true"),
+     *              @OA\Property (property="message", type="string", example="Category deleted"),
+     *          ),
+     *     ),
+     *
+     *)
+     *
+     */
+    public function delete(Request $request)
+    {
+        $this->validate($request, [
+            'id' => 'required|numeric',
+        ]);
+
+        if (!$this->categoryRepository->find($request->id)) {
+            return $this->respondNotFound('دسته بندی وجود ندارد');
+        }
+
+        if (!$this->categoryRepository->delete($request->id)) {
+            return $this->respondInternalError('دسته بندی حذف نشد');
+        }
+
+        $this->categoryRepository->delete($request->id);
+
+        return $this->respondSuccess('دسته بندی حذف شد', []);
     }
 
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,12 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     *
+     * @OA\Info (
+     *     title="7Azmoon",
+     *     version="1.0.0"
+     * )
+     *
+     */
 }
