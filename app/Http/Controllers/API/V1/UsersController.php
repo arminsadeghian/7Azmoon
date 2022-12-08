55fc525d2a8d0d52a26d0db88a016f7c521b9595
arminsadeghian <eng.a.sadeghian@gmail.com>
2022-12-08 16:34:35 +0330

    Implement documentation of users

--- app/Http/Controllers/API/V1/UsersController.php
+++ app/Http/Controllers/API/V1/UsersController.php
@@ -13,6 +13,52 @@ class UsersController extends BaseAPIController
     {
     }
 
+    /**
+     *
+     * @OA\Get (
+     *     path="/api/v1/users",
+     *     description="Returns all users",
+     *     tags={"users"},
+     *
+     *     @OA\Parameter (
+     *          name="page",
+     *          in="path",
+     *          description="Specify the page number",
+     *          required=false,
+     *          @OA\Schema (type="integer")
+     *     ),
+     *
+     *     @OA\Parameter (
+     *          name="page_size",
+     *          in="path",
+     *          description="Specify the number of results per page",
+     *          required=false,
+     *          @OA\Schema (type="integer")
+     *     ),
+     *
+     *     @OA\Response (
+     *          response=200,
+     *          description="All users",
+     *          @OA\JsonContent (
+     *              @OA\Property (property="success", type="boolean", example="true"),
+     *              @OA\Property (property="message", type="string", example="All Users"),
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="array",
+     *                  @OA\Items(
+     *                      @OA\Property(property="id", type="integer", example="1"),
+     *                      @OA\Property(property="full_name", type="string", example="Armin Sadeghian"),
+     *                      @OA\Property(property="email", type="string", example="armin@gmail.com"),
+     *                      @OA\Property(property="mobile", type="string", example="09111111111"),
+     *                      @OA\Property(property="password", type="string", example="123456"),
+     *                  )
+     *              )
+     *          ),
+     *     ),
+     *
+     *)
+     *
+     */
     public function index(Request $request)
     {
         $this->validate($request, [
@@ -25,6 +71,47 @@ class UsersController extends BaseAPIController
         return $this->respondSuccess('کاربران', $users);
     }
 
+    /**
+     *
+     * @OA\Post (
+     *     path="/api/v1/users",
+     *     description="Create new user",
+     *     tags={"users"},
+     *
+     *     @OA\RequestBody (
+     *          required=true,
+     *          description="Create new user",
+     *          @OA\JsonContent (
+     *                 @OA\Property(property="full_name", type="string", example="Armin Sadeghian"),
+     *                 @OA\Property(property="email", type="string", example="armin@gmail.com"),
+     *                 @OA\Property(property="mobile", type="string", example="09111111111"),
+     *                 @OA\Property(property="password", type="string", example="123456"),
+     *          ),
+     *      ),
+     *
+     *     @OA\Response (
+     *          response=201,
+     *          description="User created",
+     *          @OA\JsonContent (
+     *              @OA\Property (property="success", type="boolean", example="true"),
+     *              @OA\Property (property="message", type="string", example="User created"),
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="array",
+     *                  @OA\Items(
+     *                          @OA\Property(property="id", type="integer", example="1"),
+     *                          @OA\Property(property="full_name", type="string", example="Armin Sadeghian"),
+     *                          @OA\Property(property="email", type="string", example="armin@gmail.com"),
+     *                          @OA\Property(property="mobile", type="string", example="09111111111"),
+     *                          @OA\Property(property="password", type="string", example="123456"),
+     *                  )
+     *              )
+     *          ),
+     *     ),
+     *
+     * )
+     *
+     */
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -51,6 +138,46 @@ class UsersController extends BaseAPIController
         ]);
     }
 
+    /**
+     *
+     * @OA\Put (
+     *     path="/api/v1/users",
+     *     description="Update user information",
+     *     tags={"users"},
+     *
+     *     @OA\RequestBody (
+     *          required=true,
+     *          description="Update user information",
+     *          @OA\JsonContent (
+     *              @OA\Property(property="id", type="integer", example="1"),
+     *              @OA\Property(property="full_name", type="string", example="Armin Sadeghian"),
+     *              @OA\Property(property="email", type="string", example="armin@gmail.com"),
+     *              @OA\Property(property="mobile", type="string", example="09111111111"),
+     *          ),
+     *      ),
+     *
+     *     @OA\Response (
+     *          response=200,
+     *          description="User information updated",
+     *          @OA\JsonContent (
+     *              @OA\Property (property="success", type="boolean", example="true"),
+     *              @OA\Property (property="message", type="string", example="User information updated"),
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="array",
+     *                  @OA\Items(
+     *                      @OA\Property(property="id", type="integer", example="1"),
+     *                      @OA\Property(property="full_name", type="string", example="Armin Sadeghian"),
+     *                      @OA\Property(property="email", type="string", example="armin@gmail.com"),
+     *                      @OA\Property(property="mobile", type="string", example="09111111111"),
+     *                  )
+     *              )
+     *          ),
+     *     ),
+     *
+     * )
+     *
+     */
     public function updateInformation(Request $request)
     {
         $this->validate($request, [
@@ -73,6 +200,46 @@ class UsersController extends BaseAPIController
         ]);
     }
 
+    /**
+     *
+     * @OA\Put (
+     *     path="/api/v1/users/change-password",
+     *     description="Update user password",
+     *     tags={"users"},
+     *
+     *     @OA\RequestBody (
+     *          required=true,
+     *          description="Update user password",
+     *          @OA\JsonContent (
+     *              @OA\Property(property="id", type="integer", example="1"),
+     *              @OA\Property(property="password", type="string", example="123456"),
+     *              @OA\Property(property="password_repeat", type="string", example="123456"),
+     *          ),
+     *      ),
+     *
+     *     @OA\Response (
+     *          response=200,
+     *          description="User password updated",
+     *          @OA\JsonContent (
+     *              @OA\Property (property="success", type="boolean", example="true"),
+     *              @OA\Property (property="message", type="string", example="User password updated"),
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="array",
+     *                  @OA\Items(
+     *                      @OA\Property(property="id", type="integer", example="1"),
+     *                      @OA\Property(property="full_name", type="string", example="Armin Sadeghian"),
+     *                      @OA\Property(property="email", type="string", example="armin@gmail.com"),
+     *                      @OA\Property(property="mobile", type="string", example="09111111111"),
+     *                      @OA\Property(property="password", type="string", example="123456"),
+     *                  )
+     *              )
+     *          ),
+     *     ),
+     *
+     * )
+     *
+     */
     public function updatePassword(Request $request)
     {
         $this->validate($request, [
@@ -97,6 +264,33 @@ class UsersController extends BaseAPIController
 
     }
 
+    /**
+     *
+     * @OA\Delete  (
+     *     path="/api/v1/users",
+     *     description="Delete a user",
+     *     tags={"users"},
+     *
+     *     @OA\RequestBody (
+     *          required=true,
+     *          description="Delete a user",
+     *          @OA\JsonContent (
+     *              @OA\Property (property="id", type="integer", example="1"),
+     *          ),
+     *      ),
+     *
+     *     @OA\Response (
+     *          response=200,
+     *          description="User deleted",
+     *          @OA\JsonContent (
+     *              @OA\Property (property="success", type="boolean", example="true"),
+     *              @OA\Property (property="message", type="string", example="User deleted"),
+     *          ),
+     *     ),
+     *
+     *)
+     *
+     */
     public function delete(Request $request)
     {
         $this->validate($request, [
